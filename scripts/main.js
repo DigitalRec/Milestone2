415cfb8da5a8f5c73b7b47a7ffa37ca12f563194
chiyin1990 <36976283+chiyin1990@users.noreply.github.com>
2018-04-26 20:45:13 +0930

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+window.onscroll = function(){ sticknav() };
+var navbar= document.getElementById("d_nav");
+var sticky = navbar.offsetTop;
+
+function sticknav() {
+  if(window.pageYOffset >= sticky){
+    navbar.classList.add("stick");
+  } else {
+    navbar.classList.remove("stick");
+  }
+}
+
+
+
+function mobilenav_open(){
+  document.getElementById("sidemenu").style.width="100%";
+}
+function mobilenav_close(){
+  document.getElementById("sidemenu").style.width = "0";
+}
+
+
+var dropdown = document.getElementsByClassName("side_dropdown_btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
